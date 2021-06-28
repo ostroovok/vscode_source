b6f77fc82684aaf62a69a4acaf9276cd9b42e0bb
e.barkalov <barkalov125@mail.ru>
2021-06-28 10:52:55 +0300

    Исправил 1 таск

--- QPD_js_Tasks/Theme1/task1.js
+++ QPD_js_Tasks/Theme1/task1.js
@@ -1,15 +1,16 @@
 
-let arr = [1,1,1,2,2,2,3,3,3,4,4,5,5,5,5]
+let arr = [1,1,1,2,2,2,3,3,3,4,4,5,5,5,5,5,5,5,5,5,6]
 
 arr = sortedUniq(arr)
 console.log(arr)
 
 function sortedUniq(array){
     let outList = []
-    for (let index = 0; index < array.length; index++) {
-        if(array[index] !== array[index - 1]){
+    for (let index = 0; index < array.length - 1; index++) {
+        if(array[index] !== array[index + 1]){
             outList.push(array[index])
         }
     }
+    outList.push(array[array.length-1])
     return outList;
 }
