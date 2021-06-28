0d4c6fb5a5fed38c11ef5094ba45c91cff79078b
e.barkalov <barkalov125@mail.ru>
2021-06-28 11:36:25 +0300

    подправил внешний вид

--- QPD_js_Tasks/Theme1/task2.js
+++ QPD_js_Tasks/Theme1/task2.js
@@ -4,13 +4,14 @@ console.log('Simple Test: ' + isEqual('11', '11'))
 
 console.log('Objects tests: ')
 console.log(isEqual(NaN, {foo: {obj1 : 'boo'}}))
+console.log(isEqual({obj1 : 'boo'}, {obj1 : 'boo'}))
 console.log(isEqual({foo: {obj1 : 'boo'}}, {foo: {obj1 : 'boo'}}))
 console.log(isEqual({foo: {obj1 : 'to'}}, {foo: {obj1 : 'boo'}}))
-console.log(isEqual([1,2,3],[1,2,3]))
 
+console.log('Array tests: ')
+console.log(isEqual([1,2,3],[1,2,3]))
 console.log(isEqual(NaN,[1,2,3]))
 console.log(isEqual({x:1},[1,2,3]))
-
 console.log(isEqual([1,2,3],[1,2,3,4]))
 
 function isEqual(value, other){
