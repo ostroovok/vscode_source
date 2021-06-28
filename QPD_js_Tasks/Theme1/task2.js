3a0a7310b9ca37563e45635aa765772a41ec4e8d
e.barkalov <barkalov125@mail.ru>
2021-06-28 11:34:19 +0300

    исправил ошибки

--- QPD_js_Tasks/Theme1/task2.js
+++ QPD_js_Tasks/Theme1/task2.js
@@ -6,10 +6,12 @@ console.log('Objects tests: ')
 console.log(isEqual(NaN, {foo: {obj1 : 'boo'}}))
 console.log(isEqual({foo: {obj1 : 'boo'}}, {foo: {obj1 : 'boo'}}))
 console.log(isEqual({foo: {obj1 : 'to'}}, {foo: {obj1 : 'boo'}}))
-console.log(_.isEqual([1,2,3],[1,2,3]))
-console.log(_.isEqual([1,2,3,4],[1,2,3]))
-console.log(_.isEqual(NaN,[1,2,3]))
-console.log(_.isEqual({x:1},[1,2,3]))
+console.log(isEqual([1,2,3],[1,2,3]))
+
+console.log(isEqual(NaN,[1,2,3]))
+console.log(isEqual({x:1},[1,2,3]))
+
+console.log(isEqual([1,2,3],[1,2,3,4]))
 
 function isEqual(value, other){
     
@@ -24,6 +26,9 @@ function isEqual(value, other){
         return value === other;
     }
     else if(Array.isArray(value) && Array.isArray(other)){
+        if(value.length !== other.length){
+            return false;
+        }
         for (let index = 0; index < value.length; index++) {
             if(value[index] !== other[index]){
                 return false;
