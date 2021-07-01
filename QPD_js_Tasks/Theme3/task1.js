8129c4fcd7481b966de10e72ff13a20d6e108345
e.barkalov <barkalov125@mail.ru>
2021-07-01 16:25:00 +0300

    исправил random

--- QPD_js_Tasks/Theme3/task1.js
+++ QPD_js_Tasks/Theme3/task1.js
@@ -1,7 +1,7 @@
 arr = [1,2,3,4,5,6,7,8]
 
 Array.prototype.random = function(){
-    let index = _.random(this.length-1)
+    let index = Math.floor(Math.random() * arr.length)
     return this[index];
 }
 
